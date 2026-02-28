fae18901ba7f8c9e3ec9f3545ad02c5b9f1d72ae
vaishnavi <vudayagirivaishnavi02@gmail.com>
2026-02-28 13:02:09 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,44 @@
+const employees = [
+  {
+    eno: 101,
+    name: "Ravi",
+    marks: [78, 82, 91],
+  },
+  {
+    eno: 102,
+    name: "Bhanu",
+    marks: [65, 70, 68],
+  },
+  {
+    eno: 103,
+    name: "Sneha",
+    marks: [88, 92, 95],
+  },
+  {
+    eno: 104,
+    name: "Kiran",
+    marks: [55, 60, 58],
+  },
+  {
+    eno: 105,
+    name: "Anitha",
+    marks: [90, 85, 87],
+  },
+];
+
+//1. Insert new Emp at 2nd positi
+employees.splice(2,0,{eno: 106,
+    name: "tan",
+    marks: [78, 45, 99]})
+  console.log(employees)
+
+//2. Remove an emp with name "Kiran"
+ employees.splice(4,1)
+  console.log(employees)
+//3.Change the last mark 95 to 75 of emp  "Sneha"
+employees.splice(3,1,{
+    eno: 103,
+    name: "Sneha",
+    marks: [88, 92, 75],
+  })
+  console.log(employees)
